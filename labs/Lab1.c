d0dfb31f8232b716918774fbde737a6aa06e894b
Ryang Sohn <loop.infinitely@gmail.com>
2022-09-07 10:30:24 +0900

    Complete lab 1

---
+++ labs/Lab1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  printf("===================================\n");
+  printf(" RRRR   Y   Y   AAA   N   N   GGG  \n");
+  printf(" R   R   Y Y   A   A  NN  N  G     \n");
+  printf(" RRRR     Y    AAAAA  N N N  G GGG \n");
+  printf(" R  R     Y    A   A  N  NN  G   G \n");
+  printf(" R   R    Y    A   A  N   N   GGG  \n");
+  printf("===================================\n");
+  return 0;
+}
