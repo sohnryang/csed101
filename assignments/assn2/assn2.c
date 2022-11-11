a654500fb583384ef52c430656bf6fa5a381dbc2
Ryang Sohn <loop.infinitely@gmail.com>
2022-11-11 15:40:13 +0900

    Add comments

--- assignments/assn2/assn2.c
+++ assignments/assn2/assn2.c
@@ -25,8 +25,8 @@
 #define OP_EXIT 5
 
 // Consts for results.
-#define RES_SUCCESS 0
-#define RES_ERROR 1
+#define RES_SUCCESS 0 // the function was successful
+#define RES_ERROR 1   // the function errored out
 
 // Return maximum value.
 float max_f(float a, float b);
@@ -88,11 +88,15 @@ int choose_menu(const char *filename);
 void choose_colors(int *source, int *target);
 
 int main() {
-  char filename[MAX_PATH_LEN + 1];
+  char filename[MAX_PATH_LEN + 1]; // max length + null terminal
   printf("Enter input filename: ");
   scanf("%s", filename);
+
+  // Using dynamic memory allocation might be a better idea.
+  // Array dimensions are (C, H, W).
   int image_rgb[3][SIZE][SIZE], image_width, image_height;
   float image_hsv[3][SIZE][SIZE];
+
   int load_result =
       load_image(filename, image_rgb, image_hsv, &image_width, &image_height);
   if (load_result == RES_ERROR) {
@@ -112,6 +116,7 @@ int main() {
       print_histogram(image_hsv, image_width, image_height);
       choose_colors(&source, &target);
       change_color(image_hsv, image_width, image_height, source, target);
+      // The `image_hsv` array is changed, so propagate to `image_rgb`.
       hsv_to_rgb_image(image_hsv, image_rgb, image_width, image_height);
       break;
     case OP_PRINT:
@@ -151,20 +156,23 @@ int load_image(const char *filename, int image_rgb[][SIZE][SIZE],
                float image_hsv[][SIZE][SIZE], int *width, int *height) {
   FILE *image_fp = fopen(filename, "r");
   if (image_fp == NULL)
-    return RES_ERROR;
-  char signature[3]; // Must be "P3"
-  fscanf(image_fp, "%s", signature);
-  fscanf(image_fp, "%d %d", width, height);
+    return RES_ERROR; // report failure if file could not be opened
+  char signature[3];  // must be "P3"
+  fscanf(image_fp, "%s", signature); // input "P3" signature from file
+  fscanf(image_fp, "%d %d", width,
+         height); // input width and height of the image
   int channel_len;
   fscanf(image_fp, "%d", &channel_len);
   for (int y = 0; y < *height; y++)
     for (int x = 0; x < *width; x++) {
       for (int chan = 0; chan < 3; chan++)
         fscanf(image_fp, "%d", &image_rgb[chan][y][x]);
+      // The whole pixel is loaded after the above loop. Now the pixel can be
+      // converted to HSV color space.
       rgb_to_hsv(image_rgb[0][y][x], image_rgb[1][y][x], image_rgb[2][y][x],
                  &image_hsv[0][y][x], &image_hsv[1][y][x], &image_hsv[2][y][x]);
     }
-  fclose(image_fp);
+  fclose(image_fp); // clean up file pointer
   return RES_SUCCESS;
 }
 
@@ -175,7 +183,7 @@ void save_image(int image_rgb[][SIZE][SIZE], int width, int height) {
     for (int x = 0; x < width; x++)
       for (int chan = 0; chan < 3; chan++)
         fprintf(image_fp, "%d ", image_rgb[chan][y][x]);
-  fclose(image_fp);
+  fclose(image_fp); // clean up file pointer
 }
 
 void print_image(int image_rgb[][SIZE][SIZE], int width, int height) {
@@ -183,11 +191,12 @@ void print_image(int image_rgb[][SIZE][SIZE], int width, int height) {
     for (int x = 0; x < width; x++) {
       set_output_color_rgb(image_rgb[0][y][x], image_rgb[1][y][x],
                            image_rgb[2][y][x]);
-      printf("■");
+      printf("■"); // each pixel is printed as a colored square
     }
-    printf("\n");
+    printf("\n"); // put a newline after a single line of the image is printed
   }
-  reset_output_color();
+  reset_output_color(); // reset foreground color after dumping image to the
+                        // terminal
 }
 
 void print_histogram(float image_hsv[][SIZE][SIZE], int width, int height) {
@@ -195,15 +204,18 @@ void print_histogram(float image_hsv[][SIZE][SIZE], int width, int height) {
   for (int y = 0; y < height; y++)
     for (int x = 0; x < width; x++)
       pixel_count[quantize_hue(image_hsv[0][y][x])] +=
-          !abs_equal(image_hsv[1][y][x], 0);
+          !abs_equal(image_hsv[1][y][x], 0); // colors with s=0 are ignored
   for (int i = 0; i < 12; i++) {
     printf("[%2d]", i);
     int r, g, b;
+    // Pre-calculating lookup tables might be a better idea.
     hsv_to_rgb(30 * i, 1, 1, &r, &g, &b);
     set_output_color_rgb(r, g, b);
+
+    // The length of histogram bars are one-tenth of the actual count.
     for (int j = 0; j < pixel_count[i] / 10; j++)
       printf("■");
-    reset_output_color();
+    reset_output_color(); // reset color for prints afterwards
     printf("%d\n", pixel_count[i]);
   }
 }
@@ -215,38 +227,37 @@ void rgb_to_hsv(int r, int g, int b, float *h, float *s, float *v) {
   float c_max = max_f(max_f(normalized_r, normalized_g), normalized_b);
   float c_min = min_f(min_f(normalized_r, normalized_g), normalized_b);
   float delta = c_max - c_min;
-  float h_val, s_val, v_val;
   if (abs_equal(delta, 0))
-    h_val = 0;
-  else if (abs_equal(c_max, normalized_r))
-    h_val = 60 * (normalized_g - normalized_b) / delta;
-  else if (abs_equal(c_max, normalized_g))
-    h_val = 60 * ((normalized_b - normalized_r) / delta + 2);
+    *h = 0;
+  // Error-tolerant comparison is not used because `c_max` is exactly same as
+  // one of the `normalized_*` variable.
+  else if (c_max == normalized_r)
+    *h = 60 * (normalized_g - normalized_b) / delta;
+  else if (c_max == normalized_g)
+    *h = 60 * ((normalized_b - normalized_r) / delta + 2);
   else
-    h_val = 60 * ((normalized_r - normalized_g) / delta + 4);
+    *h = 60 * ((normalized_r - normalized_g) / delta + 4);
+
   if (abs_equal(c_max, 0))
-    s_val = 0;
+    *s = 0;
   else
-    s_val = delta / c_max;
-  v_val = c_max;
-  if (compare_f(h_val, 0) < 0)
-    h_val += 360;
-  if (compare_f(h_val, 360) >= 0)
-    h_val -= 360;
-  if (h != NULL)
-    *h = h_val;
-  if (s != NULL)
-    *s = s_val;
-  if (v != NULL)
-    *v = v_val;
+    *s = delta / c_max;
+  *v = c_max;
+
+  // Normalize hue value. Using `fmod` might be a better idea.
+  if (compare_f(*h, 0) < 0)
+    *h += 360;
+  if (compare_f(*h, 360) >= 0)
+    *h -= 360;
 }
 
 int quantize_hue(float h) {
   float q = h / 30;
   for (int i = 0; i < 11; i++)
-    if (compare_f(q, i) >= 0 && compare_f(q, i + 1) < 0)
+    if (compare_f(q, i) >= 0 &&
+        compare_f(q, i + 1) < 0) // use error-tolerant comparison
       return i;
-  return 11;
+  return 11; // if nothing matched, the hue must be quantized to 11
 }
 
 void change_color(float image_hsv[][SIZE][SIZE], int width, int height,
@@ -254,12 +265,13 @@ void change_color(float image_hsv[][SIZE][SIZE], int width, int height,
   int delta = target - source;
   for (int y = 0; y < height; y++)
     for (int x = 0; x < width; x++) {
-      float *h = &image_hsv[0][y][x];
-      *h += delta * 30;
+      float *h = &image_hsv[0][y][x]; // only hue value is necessary
+      *h += delta * 30;               // change color
+      // Normalize hue value.
       if (compare_f(*h, 0) < 0)
-        *h += 360;
+        *h += 360; // if hue is negative, add 360
       else if (compare_f(*h, 360) >= 0)
-        *h -= 360;
+        *h -= 360; // if hue is greater or equal to 360, substract it
     }
 }
 
@@ -269,14 +281,14 @@ void hsv_to_rgb(float h, float s, float v, int *r, int *g, int *b) {
   float m = v - c;
   float normalized[3] = {0};
   int quantized_h = quantize_hue(h);
+  // Exploit symmetry to eliminate braches, which are detrimental to
+  // performance.
+  // By arranging
   normalized[(quantized_h / 2 + 1) % 6 / 2] = c;
   normalized[2 - (quantized_h / 2 + 1) % 3] = x;
-  if (r != NULL)
-    *r = (normalized[0] + m) * 255;
-  if (g != NULL)
-    *g = (normalized[1] + m) * 255;
-  if (b != NULL)
-    *b = (normalized[2] + m) * 255;
+  *r = (normalized[0] + m) * 255;
+  *g = (normalized[1] + m) * 255;
+  *b = (normalized[2] + m) * 255;
 }
 
 void hsv_to_rgb_image(float image_hsv[][SIZE][SIZE],
@@ -303,10 +315,12 @@ int choose_menu(const char *filename) {
   printf("  4. Save Image\n");
   printf("  5. Exit\n");
   printf("Loaded file: %s\n\n", filename);
+
   int input;
-  while (true) {
+  while (true) { // loop until valid input
     printf("Choose menu number >> ");
     scanf("%d", &input);
+    // Check if command is in the range.
     if (in_range(input, OP_HIST, OP_EXIT + 1))
       break;
     printf("Wrong input!\n");
@@ -315,14 +329,14 @@ int choose_menu(const char *filename) {
 }
 
 void choose_colors(int *source, int *target) {
-  while (true) {
+  while (true) { // loop until valid first color input
     printf("Choose source color >> ");
     scanf("%d", source);
     if (in_range(*source, 0, 12))
       break;
     printf("Wrong input!\n");
   }
-  while (true) {
+  while (true) { // loop until valid second color input
     printf("Choose target color >> ");
     scanf("%d", target);
     if (in_range(*target, 0, 12))
