280f3749ff4667a0f76addf85acb003b157303d2
Ryang Sohn <loop.infinitely@gmail.com>
2022-11-28 03:00:02 +0900

    Add some comments

--- assignments/assn3/assn3.c
+++ assignments/assn3/assn3.c
@@ -22,24 +22,51 @@
 #define COLOR_BLUE 94
 #define COLOR_PURPLE 95
 
+// Set terminal output color according to color code.
 void set_output_color(int code);
+
+// Reset terminal output color.
 void reset_output_color();
+
+// Clear the terminal.
 void clear_screen();
+
+// Get input from menu.
 int menu_input();
+
+// Allocate 2d array using `calloc`.
 int **alloc_2d(int height, int width);
+
+// Get ladder info from user input.
 void ladder_input(int *parties, int *height, int *line_count, char *filename);
+
+// Draw vertical line to `board` for `party` number of people and height
+// `height`.
 void draw_vertical(int **board, int parties, int height);
+
+// Randomly generate ladder for `party` number of people with height `height`.
 void generate_ladder(int **board, int num_line, int parties, int height);
+
+// Save ladder to `filename`.
 void save_ladder(char filename[], int **board, int parties, int height,
                  int num_line);
+
+// Deallocate 2d array with height `height`.
 void free_ladder(int **board, int height);
+
+// Load ladder from `filename`. Save relevant infos to variables referenced by
+// `parites`, `height`, `num_line`. Return `NULL` if `fopen` failed.
 int **load_ladder(char *filename, int *parties, int *height, int *num_line);
+
+// Print ladder saved in board. Colors are set according to `visited_map`.
 void print_ladder(int **board, int **visited_map, int parties, int height);
+
+// Traverse one step when current position is
 void traverse_step(int **board, int **visited_map, int parties, int height,
                    int *y, int *x, int source);
 
 int main() {
-  srand(time(NULL));
+  srand(time(NULL)); // set random seed to current time
   int **ladder_board;
   while (true) {
     int cmd = menu_input();
@@ -49,9 +76,12 @@ int main() {
       printf("\n");
       ladder_input(&parties, &height, &line_count, filename);
       printf("\n");
+      // Each line contains `parties` slots for vertical lines, and `parties -
+      // 1` slots for horizontal lines.
       ladder_board = alloc_2d(height, parties * 2 - 1);
       generate_ladder(ladder_board, line_count, parties, height);
       save_ladder(filename, ladder_board, parties, height, line_count);
+      // `ladder_board` will be unused, so deallocate it.
       free_ladder(ladder_board, height);
     } else if (cmd == OP_RUN) {
       printf("\n");
@@ -60,6 +90,7 @@ int main() {
       scanf("%s", filename);
       printf("\n");
       int parties, height, num_line;
+      // Load ladder from `filename`.
       ladder_board = load_ladder(filename, &parties, &height, &num_line);
       if (ladder_board == NULL) {
         printf("File not found: %s\n\n", filename);
@@ -69,7 +100,7 @@ int main() {
           current_source = 0;
       bool print_full = false;
       while (true) {
-        clear_screen();
+        clear_screen(); // clear screen before printing current turn
         if (y != -1) {
           printf("%d %d\n", y, x);
         }
@@ -77,9 +108,14 @@ int main() {
         printf("\n");
         if (y <= 0) {
           if (current_source) {
+            // If the ladder was fully painted in the last pass, results should
+            // be printed differently.
             if (print_full) {
               print_full = false;
               char *dests = calloc(parties + 1, sizeof(char));
+              // The top level does not contain horizontal lines, so
+              // destinations can be determined from the top level of
+              // `visited_map`.
               for (int i = 0; i < parties; i++)
                 dests[visited_map[0][2 * i]] = 'A' + i;
               for (int i = 1; i <= parties; i++)
@@ -88,32 +124,45 @@ int main() {
             } else
               printf("result: %d -> %c\n\n", current_source, 'A' + x / 2);
           }
+          // Each line contains `parties` slots for vertical lines, and `parties
+          // - 1` slots for horizontal lines.
           int width = 2 * parties - 1;
+          // Mark already visited cells as stale.
           for (int y = 0; y < height; y++)
             for (int x = 0; x < width; x++)
-              visited_map[y][x] = -visited_map[y][x];
+              if (visited_map[y][x] > 0)
+                visited_map[y][x] = -visited_map[y][x];
           printf(">> ");
           scanf("%d", &current_source);
+          // If `current_source` is zero even after input, it is terminal sign.
           if (current_source == 0) {
             printf("\n");
             break;
           }
+          // If `current_source` is -1, fully paint the ladder.
           if (current_source == -1) {
+            // Deallocate current `visited_map` and create a new one.
             free_ladder(visited_map, height);
+            // Each line contains `parties` slots for vertical lines, and
+            // `parties - 1` slots for horizontal lines.
             visited_map = alloc_2d(height, parties * 2 - 1);
+            // Traverse every party.
             for (int i = 0; i < parties; i++) {
               y = height;
               x = i * 2;
+              // Loop until current position is top.
               while (y > 0)
                 traverse_step(ladder_board, visited_map, parties, height, &y,
                               &x, i + 1);
             }
-            current_source = -1;
+            // Set current position to the origin.
             y = 0;
             x = 0;
+            // Print all source-destination pair.
             print_full = true;
             continue;
           }
+          // Start from the selected position.
           y = height;
           x = (current_source - 1) * 2;
         } else {
@@ -127,6 +176,8 @@ int main() {
         traverse_step(ladder_board, visited_map, parties, height, &y, &x,
                       current_source);
       }
+      // `ladder_board` and `visited_map` are unused from now, so deallocate
+      // them.
       free_ladder(ladder_board, height);
       free_ladder(visited_map, height);
     } else if (cmd == OP_EXIT) {
@@ -141,7 +192,9 @@ void set_output_color(int code) { printf("\033[%dm", code); }
 
 void reset_output_color() { printf("\033[0m"); }
 
-void clear_screen() { system("clear"); }
+void clear_screen() {
+  system("clear"); // for UNIX OSes
+}
 
 int **alloc_2d(int height, int width) {
   int **board;
